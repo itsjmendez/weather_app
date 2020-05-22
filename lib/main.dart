2322164e3ee2bb74d98c313efd7198b93924eec6
Josemanuel Mendez <jmocontactme@gmail.com>
2020-05-22 13:38:49 -0500

    chore: return a MaterialAp that contains theme and home.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:weatherapp/screens/loading_screen.dart';
 
 void main() => runApp(MyApp());
   
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      theme:  ThemeData.dark(),
+      home: LoadingScreen(),
+    );
   }
 }
 
