4647d26476c52bbed0ed03fdf57b87e882b05ff8
Josemanuel Mendez <jmocontactme@gmail.com>
2020-05-26 21:07:35 -0500

    chore: move code to class.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:weatherapp/services/location.dart';
-import 'package:http/http.dart' as http;
+
+
 const apiKey = 'b0552d25ac0aa34d32167cef0e16ce0e';
 
 
@@ -17,10 +19,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     super.initState();
-    getLocation();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
 
@@ -29,32 +30,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
 //    print(location.latitude);
 //    print(location.longitude);
-
-    getData();
   }
 
-  void getData() async{
-    http.Response response =  await http.get('https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
-     if (response.statusCode == 200){
-       String data = response.body;
-       print(data);
-
-//       var decodedData = jsonDecode(data);
-//       double temperature = decodedData['main']['temp'];
-//       int condition = decodedData ['weather'][0]['id'];
-//       String cityName = decodedData['name'];
-
-//       print(temperature);
-//       print(condition);
-//       print(cityName);
-     }
-     else{
-      print(response.statusCode);
-    }
-
-  }
-  
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold();
