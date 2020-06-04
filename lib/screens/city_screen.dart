021ab5df49f07e6abd95323aee8ef5e4387a8fd5
Josemanuel Mendez <jmocontactme@gmail.com>
2020-06-03 22:27:02 -0500

    chore: refactor Text Field's input decoration with a constant.

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -39,22 +39,9 @@ class _CityScreenState extends State<CityScreen> {
                   style: TextStyle(
                     color: Colors.blueGrey,
                   ),
-                  decoration: InputDecoration (
-                    filled: true,
-                    fillColor: Colors.white,
-                    icon: Icon(Icons.location_city,
-                    color: Colors.white,),
-                    hintText: 'Enter City Name',
-                    hintStyle: TextStyle(
-                      color:  Colors.grey,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      borderSide: BorderSide.none,
-                    )
+                  decoration: kTextFieldInputDecoration,
                   ),
                 ),
-              ),
               FlatButton(
                 onPressed: () {},
                 child: Text(
