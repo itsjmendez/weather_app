dc398f15ed99f75ccaa4e07a21f778cc1facc75d
Josemanuel Mendez <jmocontactme@gmail.com>
2020-06-03 22:39:11 -0500

    chore: pop city screen and send data to loading screen.

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -75,8 +75,8 @@ class _LocationScreenState extends State<LocationScreen> {
                     ),
                   ),
                   FlatButton(
-                    onPressed: () {
-                      Navigator.push(
+                    onPressed: () async {
+                      var typedName = await Navigator.push(
                         context,
                         MaterialPageRoute(builder: (context) => CityScreen()),
                       );
