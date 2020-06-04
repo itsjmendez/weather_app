01f8ae1c05a65e4e8d289f4b6cddbb72e5bc39d2
Josemanuel Mendez <jmocontactme@gmail.com>
2020-06-03 22:19:45 -0500

    chore: change the color of the TextField input's text.

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -36,6 +36,9 @@ class _CityScreenState extends State<CityScreen> {
               Container(
                 padding: EdgeInsets.all(20.0),
                 child: TextField(
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                  ),
                   decoration: InputDecoration (
                     filled: true,
                     fillColor: Colors.white,
